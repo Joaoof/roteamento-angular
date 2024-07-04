cb245a8b542aea1947665e5d3d2d0c3281e9f695
Joaoof <joaodeus400@gmail.com>
2024-07-04 10:00:42 -0300

    feat: add roteamento angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimeiraPaginaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
